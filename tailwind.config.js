a0fd5dc349d36d9be9675930702464b49d437f1d
Jared Connor <jaredconnor301@gmail.com>
2023-03-15 03:50:03 +0000

    Update to main container size

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,5 +20,24 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp'),
+    function({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1280px',
+          },
+          '@screen xl': {
+            maxWidth: '1400px',
+          },
+        }
+      })
+    }
   ]
 }
